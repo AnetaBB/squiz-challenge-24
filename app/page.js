c7af7236a4e60ff44c21f9e9f3e21db9aac154ef
Aneta <a.banas.brejt@gmail.com>
2024-10-23 21:05:37 +0200

    main page layout setup

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div>kiahfckdhv</div>;
+  return (
+    <div className='flex flex-col '>
+      <div className=''>Control panel</div>
+      <div className=''>List of items</div>
+    </div>
+  );
 }
