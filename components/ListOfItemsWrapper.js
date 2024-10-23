d6502a5ae873a085ab1448a54103926d08fee687
Aneta <a.banas.brejt@gmail.com>
2024-10-23 22:34:12 +0200

    fill table with data

--- components/ListOfItemsWrapper.js
+++ components/ListOfItemsWrapper.js
@@ -1,4 +1,31 @@
 export const ListOfItemsWrapper = ({ data }) => {
-  console.log(data);
-  return <>ListOfItemsWrapper</>;
+  const columnHeaders = Object.keys(data[0]);
+  console.log(data[0], columnHeaders);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            {columnHeaders.map((columnHeader) => (
+              <th key={`key-${columnHeader}`}>{columnHeader}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((dataRow) => {
+            return (
+              <tr key={`rowKey-${dataRow.id}`}>
+                {columnHeaders.map((dataItem) => {
+                  return (
+                    <td key={`dataKey-${dataRow.id}`}>{dataRow[dataItem]}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
